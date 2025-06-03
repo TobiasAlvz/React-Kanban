3b7b10ff4c0ffadb389a88beff8c14e3c02af157
Tobias Alves <tobiasjose12@gmail.com>
2025-06-03 16:34:09 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import { Box, Heading } from "@radix-ui/themes";
+import { CreateTaskForm } from "./components/CreateTaskForm";
+import { TaskBoard } from "./components/TaskBoard.tsx";
+import { Box, Flex, Heading } from "@radix-ui/themes";
 
 function App() {
   return (
-    <Box>
-      <Heading>React Kanban</Heading>
+    <Box maxWidth="80rem" mx="auto">
+      <Box height="4rem" px="4">
+        <Flex align="center" gap="4" height="100%">
+          <Heading size="8" weight="light">
+            React Kanban
+          </Heading>
+          <CreateTaskForm />
+        </Flex>
+      </Box>
+
+      <Box px="4" maxWidth="80rem" mx="auto">
+        <h2 className="mb-3">Quadro de tarefas</h2>
+        <TaskBoard />
+      </Box>
     </Box>
   );
 }
